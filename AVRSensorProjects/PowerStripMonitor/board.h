b00ea13ac56ccc273e6f519d00723ccd9bf2031f
Kevin Weekly <kweekly@eecs.berkeley.edu>
2013-01-22 23:05:48 -0800

    yadda

--- AVRSensorProjects/PowerStripMonitor/board.h
+++ AVRSensorProjects/PowerStripMonitor/board.h
@@ -9,6 +9,18 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
+// output (need to send message to mote)
+#define MOTE_RX_RTSN
+
+// input (mote ready to receive)
+#define MOTE_RX_CTSN
+
+// input (mote has a message)
+#define MOTE_TX_RTSN
+
+// output (allow mote to send message)
+#define MOTE_TX_CTSN
+
 
 
 
